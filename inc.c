a9df776ff80be5f4d40ecb4352320784b671ab4c
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2022-11-06 10:12:48 -0600

    initial commit

---
+++ inc.c
@@ -0,0 +1,23 @@
+#include <gmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    mpz_t n;
+    unsigned long int i;
+
+    for (i = 1; i < ULLONG_MAX; i++) {
+        printf("%lu\n", i);
+    }
+
+    mpz_init(n);
+    mpz_set_ui(n, i + 1);
+    for (;;) {
+        mpz_out_str(stdout, 10, n);
+        puts("");
+        mpz_add_ui(n, n, 1);
+    }
+    mpz_clear(n);
+    return 0;
+}
